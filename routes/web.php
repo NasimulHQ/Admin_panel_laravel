2cd8c75361fa853a8f9232d069df0a760d1a8c86
NasimulHQ <nasimul345@gmail.com>
2022-08-28 00:17:41 +0600

    feat: first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('forget_password');
+
+
